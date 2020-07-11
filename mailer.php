5bdd79410c806111fdbfc35c7ee83196a62e6560
Gotechignite <gotechignite@gmail.com>
2020-07-11 01:16:56 -1200

    php change

--- mailer.php
+++ mailer.php
@@ -8,7 +8,7 @@
 
     // Check the data.
     if (empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        header("Location: http://localhost/Farm-website/index.php?success=-1#form");
+        header("Location: https://agrotrendfarm.herokuapp.com/index.php?success=-1#form");
         exit;
     }
 
@@ -31,6 +31,6 @@
     mail($recipient, $subject, $email_content, $email_headers);
     
     // Redirect to the index.html page with success code
-    header("Location: http://localhost/Farm-website/index.php?success=1#form");
+    header("Location: https://agrotrendfarm.herokuapp.com/index.php?success=1#form");
 
 ?>

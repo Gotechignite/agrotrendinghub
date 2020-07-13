58bce373c1ee4a9f2dca654bc02bfbc341c0fe81
Gotechignite <gotechignite@gmail.com>
2020-07-13 23:31:26 +0100

    updated changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,21 +134,6 @@
     $('#topbar').addClass('topbar-scrolled');
   }
 
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-
-  $('.back-to-top').click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-    }, 1500, 'easeInOutExpo');
-    return false;
-  });
 
   // jQuery counterUp
   $('[data-toggle="counter-up"]').counterUp({
